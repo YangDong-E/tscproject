6965962f1e7c088658c5f0bf18be5c925b5bad72
YangDong-E <ehddh125@gmail.com>
2024-04-03 21:33:32 +0900

    24.04.03 - 프로젝트 코드 교체

---
+++ asia/frontend/src/App.tsx
@@ -0,0 +1,34 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="d-flex flex-column vh-100">
+      <header>
+        <Navbar bg="dark">
+          <Container>
+            <Navbar.Brand>아시아 전기</Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href="/cart" className="nav-link">
+              Cart
+            </a>
+            <a href="/signin" className="nav-link">
+              Sign
+            </a>
+          </Nav>
+        </Navbar>
+      </header>
+      <main>
+        <Container className="mt-3">
+          <Outlet />
+        </Container>
+      </main>
+      <footer>
+        <div className="text-center">All right reserved</div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ asia/frontend/src/pages/ProductPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Helmet } from "react-helmet-async";
+
+export default function ProductPage() {
+  return (
+    <div>
+      <Helmet>
+        <title>Product Page</title>
+      </Helmet>
+      ProductPage
+    </div>
+  );
+}
